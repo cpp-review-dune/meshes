26969ccd151113450fa8083ff8b2affb454da9ef
Oromion <caznaranl@uni.pe>
2023-12-27 18:09:06 -0500

    Update

---
+++ gmsh/updater.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+OLD_VERSION=$(grep 'julia_version' Manifest.toml | cut -d '"' -f 2 | tr -d '.')
+VERSION=$(julia --version)
+NEW_VERSION=$(echo "$VERSION" | sed -n 's/.*julia version \([0-9.]*\).*/\1/p' | tr -d '.')
+
+if [[ $NEW_VERSION -gt $OLD_VERSION ]]; then
+  if [ -f Project.toml ]; then
+    printf '%s\n' "Removing Lock file"
+    rm -r *.toml
+    julia -e 'using Pkg; Pkg.activate(""); Pkg.add("IJulia")'
+  fi
+fi
