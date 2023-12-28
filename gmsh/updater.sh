89ad5a58718b614a373a16da5342d1517dc7ec93
Oromion <caznaranl@uni.pe>
2023-12-27 22:54:59 -0500

    Update

--- gmsh/updater.sh
+++ gmsh/updater.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+sudo pacman -Sy archlinux-keyring --noconfirm
+sudo pacman -Su --noconfirm
+
 OLD_VERSION=$(grep 'julia_version' Manifest.toml | cut -d '"' -f 2 | tr -d '.')
 VERSION=$(julia --version)
 NEW_VERSION=$(echo "$VERSION" | sed -n 's/.*julia version \([0-9.]*\).*/\1/p' | tr -d '.')
